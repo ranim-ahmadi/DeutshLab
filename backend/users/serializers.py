9bd88afaa7ce949f127b8978d4c15199b780d123
ranim-ahmadi <ahmadiranim.pro@gmail.com>
2025-11-15 19:18:06 +0100

    Login register

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,4 +1,3 @@
-# users/serializers.py
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
@@ -9,11 +8,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = (
-            'first_name', 'last_name', 'email',
-            'password', 'password2',
-            'level'
-        )
+        fields = ('first_name', 'last_name', 'email', 'password', 'password2', 'level')
         extra_kwargs = {
             'password': {'write_only': True},
             'email': {'required': True},
@@ -30,14 +25,22 @@ class RegisterSerializer(serializers.ModelSerializer):
         validated_data.pop('password2')
         password = validated_data.pop('password')
 
+        # Génère username automatique si pas fourni
+        username = validated_data.get('username') or validated_data['email'].split('@')[0]
 
+        user = User.objects.create_user(
+            username=username,
+            email=validated_data['email'],
+            password=password,
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            level=validated_data.get('level', 'A1')
+        )
+        return user
 
 class UserSerializer(serializers.ModelSerializer):
     full_name = serializers.CharField(source='get_full_name', read_only=True)
 
     class Meta:
         model = User
-        fields = (
-            'id', 'first_name', 'last_name', 'full_name',
-            'email', 'level', 'avatar', 'streak', 'total_xp'
-        )
+        fields = ('id', 'first_name', 'last_name', 'full_name', 'email', 'level', 'avatar', 'streak', 'total_xp')
